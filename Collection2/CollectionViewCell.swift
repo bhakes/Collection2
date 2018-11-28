1627d8fed61268187602d5f3a4690683ec687b30
Ben Hakes <bhhakes@gmail.com>
2018-11-28 11:39:15 -0600

    add requiste files

---
+++ Collection2/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Collection2
+//
+//  Created by Benjamin Hakes on 11/28/18.
+//  Copyright © 2018 Benjamin Hakes. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Collection2/CrayonModel.swift
@@ -0,0 +1,9 @@
+//
+//  CrayonModel.swift
+//  Collection2
+//
+//  Created by Benjamin Hakes on 11/28/18.
+//  Copyright © 2018 Benjamin Hakes. All rights reserved.
+//
+
+import Foundation
